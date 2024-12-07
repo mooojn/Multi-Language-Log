ce0d65396d918e2beb29e5b90a80589c31608bd0
mooojn <moojntariq@gmail.com>
2024-12-07 14:30:54 +0500

    rm

--- NodeJS/chalk-pkg.js
+++
@@ -1,5 +0,0 @@
-const chalk = require('chalk');
-
-console.log(chalk.bold("hi"));
-console.log(chalk.red("hi"));
-console.log(chalk.bgBlue("hi"));
--- NodeJS/html-pages/index.js
+++
@@ -1,9 +0,0 @@
-    const express = require('express');
-    const path = require('path');
-
-    const app = express();
-    const htmlPagesPath = path.join(__dirname, '/public');
-
-    app.use(express.static(htmlPagesPath))
-
-    app.listen(4500);
